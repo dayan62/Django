5ffbeec706e37921759a57a68a441064462f2e4e
dayan62 <dayanshaikh62@gmail.com>
2024-08-25 19:05:26 +0530

    practice

---
+++ first.py
@@ -0,0 +1,10 @@
+
+a=input("Enter first no: ")
+b=input("enter Second no: ")
+
+if a<b:
+    print("first No. is greater than Second no.")
+elif a>b:
+    print("second no. is greater than First")
+else:
+    print("both no. is equal")
